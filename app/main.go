86f0ed6c39bcbedf4175bf92c5cc8080c6661d24
Numeez <numeezbaloch17@gmail.com>
2026-05-30 11:06:48 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"os"
+	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -18,18 +20,42 @@ func main() {
 		}
 		readInput := scanner.Text()
 		handleInput(readInput)
-		
+
+	}
+}
+
+func handleInput(input string) {
+	inputs := strings.Split(input, " ")
+	var command string
+	var rest []string
+	if len(inputs) > 0 {
+		command = inputs[0]
+		if len(inputs[1:])!=0{
+			rest = inputs[1:]
+		}
+	}
+	switch command {
+	case "exit":
+		os.Exit(0)
+	case "echo":
+		fmt.Fprintf(os.Stdout, "%s\n",makeString(rest))
+	default:
+		fmt.Fprintf(os.Stdout, "%s: command not found", input)
+		fmt.Fprintf(os.Stdout, "\n")
+
 	}
 }
 
 
-func handleInput(input string){
-	switch input{
-	case "exit":
-		os.Exit(0)
-	default:
-		fmt.Fprintf(os.Stdout, "%s: command not found", input)
-		fmt.Fprintf(os.Stdout,"\n")
-		
+func makeString(input []string)string{
+	var out bytes.Buffer
+	for i,str:= range input{
+		if i==len(input)-1{
+		out.WriteString(str)
+		}else{
+			out.WriteString(str)
+			out.WriteString(" ")
+		}
 	}
+	return  out.String()
 }
