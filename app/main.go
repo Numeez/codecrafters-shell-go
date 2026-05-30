894fa25471f1578b88d268fa0daa1bc218710002
Numeez <numeezbaloch17@gmail.com>
2026-05-30 10:37:25 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -10,4 +12,9 @@ var _ = fmt.Print
 func main() {
 
 	fmt.Print("$ ")
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		readInput := scanner.Text()
+		fmt.Fprintf(os.Stdout, "{%s}: command not found", readInput)
+	}
 }
